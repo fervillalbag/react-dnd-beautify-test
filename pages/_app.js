82655290cb12235903801af1dd60adee2831167e
Fernando Villalba <fernando.villalba@clt.com.py>
2023-06-16 16:23:22 -0400

    feat: start template

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../src/theme";
+import "../src/styles.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
